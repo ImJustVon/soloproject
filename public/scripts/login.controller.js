e0c93f58992268db504a77766e3a8c13e682ddfb
Vaughn Prosser <vaughnprosser@gmail.com>
2016-10-31 11:54:23 -0500

    basic routing and login/register

---
+++ public/scripts/login.controller.js
@@ -0,0 +1,20 @@
+angular.module('crankSistersApp')
+.controller('LoginController', LoginController);
+
+function LoginController($http, $location) {
+  console.log('LoginController loaded');
+  var ctrl = this;
+
+  ctrl.login = function () {
+    console.log('logging in');
+    $http.post('/login', {
+      username: ctrl.username,
+      password: ctrl.password,
+    }).then(function (response) {
+      console.log(response);
+    }, function (error) {
+
+      console.log('error loggin in', error);
+    });
+  };
+}
